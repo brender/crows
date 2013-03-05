925c066f60d06c842bd32cf03b9a46bdc41cd78c
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-04 20:53:46 -0800

    Made an alternating crow website based on time

--- main.elm
+++ main.elm
@@ -1,3 +1,14 @@
 --crows
 
-main = plainText "foo"
+main = image 600 450 <~ crows
+
+crow1 = constant "crow1.jpg"
+crow2 = constant "crow2.jpg"
+
+t = every (second * 2)
+t2 = delay second t
+
+crow1Time = sampleOn t crow1
+crow2Time = sampleOn t2 crow2
+
+crows = merge crow1Time crow2Time
