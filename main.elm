884aa4eb3cce6c4aea95db5e97abceb22e56703d
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-04 21:08:19 -0800

    Centered the crow images

--- main.elm
+++ main.elm
@@ -1,6 +1,10 @@
 --crows
 
-main = image 600 450 <~ crows
+import Time
+
+--width and height are signals
+main = lift4 container Window.width Window.height (constant middle) images
+
 
 crow1 = constant "crow1.jpg"
 crow2 = constant "crow2.jpg"
@@ -12,3 +16,4 @@ crow1Time = sampleOn t crow1
 crow2Time = sampleOn t2 crow2
 
 crows = merge crow1Time crow2Time
+images = image 600 450 <~ crows
